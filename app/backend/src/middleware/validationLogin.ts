13c9c3235ae55bb14012904ef9843eaebeb7a567
Bruno Landim <brunolandim13@hotmail.com>
2022-05-18 17:53:30 -0300

    Refatorando middleware :recycle:

--- app/backend/src/middleware/validationLogin.ts
+++ app/backend/src/middleware/validationLogin.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 
+const errMessage = { message: 'Incorrect email or password' };
+
 export const validateEmail = (req:Request, res:Response, next:NextFunction) => {
   const { email } = req.body;
   const regex = /^.+@\w+\.\w+$/;
 
   if (!email) return res.status(400).json({ message: 'All fields must be filled' });
   if (typeof email !== 'string') {
-    return res.status(401).json({ message: 'Incorrect email or password' });
+    return res.status(401).json(errMessage);
   }
   if (!regex.test(email)) {
-    return res.status(401).json({ message: 'Incorrect email or password' });
+    return res.status(401).json(errMessage);
   }
   next();
 };
@@ -18,10 +20,10 @@ export const validatePass = (req:Request, res:Response, next:NextFunction) => {
   const { password } = req.body;
   if (!password) return res.status(400).json({ message: 'All fields must be filled' });
   if (typeof password !== 'string') {
-    return res.status(401).json({ message: 'Incorrect email or password' });
+    return res.status(401).json(errMessage);
   }
   if (password.length <= 7) {
-    return res.status(401).json({ message: 'Incorrect email or password' });
+    return res.status(401).json(errMessage);
   }
   next();
 };
