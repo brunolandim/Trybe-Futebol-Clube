d1d69335c59843497836a734944fca9648a74313
Bruno Landim <brunolandim13@hotmail.com>
2022-05-19 20:45:15 -0300

    Ajustes :recycle:

--- app/backend/src/controller/userController.ts
+++ app/backend/src/controller/userController.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import UserService from '../service/userService';
 
-const service = new UserService();
-
 export default class UserController {
+  public service : UserService;
+
+  constructor() {
+    this.service = new UserService();
+  }
+
   public async login(req:Request, res:Response, next:NextFunction) {
     try {
       const { email, password } = req.body;
-      const result = await service.login(password, email);
+      const result = await this.service.login(password, email);
       if (Object.keys(result).length === 0) {
         return res.status(401).json({ message: 'Incorrect email or password' });
       }
--- app/backend/src/routes/UserRouter.ts
+++ app/backend/src/routes/UserRouter.ts
@@ -5,6 +5,11 @@ import UserController from '../controller/userController';
 const userController = new UserController();
 const userRouter: express.Router = express.Router();
 
-userRouter.post('/', validateEmail, validatePass, userController.login);
+userRouter.post(
+  '/',
+  validateEmail,
+  validatePass,
+  (req, res, next) => userController.login(req, res, next),
+);
 
 export default userRouter;
